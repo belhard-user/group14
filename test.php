23f60cbf2aaa5b50fee5c4d19a7c868467235f92
artsiom burkun <svail.13@gmail.com>
2018-02-03 20:42:26 +0300

    reg exp

--- test.php
+++ test.php
@@ -1,62 +1,12 @@
 <?php
 require_once "vendor/autoload.php";
+// https://vk.com/doc10903696_208805128?hash=ca3a558dbc4ad29d3f&dl=ec6195707f8368caf8
+$str = 'hello visit my web site http://onliner.by https://tut.by https://ya.ru';
+// echo preg_replace('@((?:ht|f)tps?://([^.]+)\.\w+)@', '<a href="$1">$2</a>', $str, -1, $c);
+$result = preg_match_all('@((?:ht|f)tps?://([^.]+)\.\w+)@', $str, $m, PREG_OFFSET_CAPTURE|PREG_SET_ORDER);
+dump($m);
+// dump($c);
+/*$re = '/php ?[57]/u';
+$str = 'i like php 5';
+preg_match($re, $str, $m);*/
 
-/*function foo($name, $id){
-    dump(func_get_args());
-}*/
-
-class People
-{
-    public $id;
-    public $name;
-
-    public function __construct($id, $name)
-    {
-        $this->id = $id;
-        $this->name = $name;
-    }
-
-    public function getFullName()
-    {
-        return sprintf(
-            '%s-%d<br>',
-            $this->name,
-            $this->id
-        );
-    }
-}
-
-
-$names = [
-    'Neo',
-    'Trinity',
-    'Tank',
-    'Morpheus',
-    'Dozer',
-    'Smith'
-];
-
-
-try{
-    $pdo = new PDO('mysql:host=localhost;dbname=site', 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-
-    /*$pdo->beginTransaction();
-    foreach($names as $name){
-        $sql = "INSERT INTO peoples (name) VALUES ('$name')";
-        $pdo->exec($sql);
-    }
-    $pdo->commit();*/
-
-    /*$sql = "SELECT name, id FROM peoples";
-
-    $peoples = $pdo->query($sql)->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'People', [1, 2]);
-
-    foreach($peoples as $people){
-        echo $people->getFullName();
-    }*/
-}catch(PDOException $e){
-    $pdo->rollBack();
-    echo $e->getMessage();
-}
